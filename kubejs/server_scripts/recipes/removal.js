a27368c964e4b2e662fd4a3631f88a0eaea2a9c6
Alex Forehand <forehand.alex@gmail.com>
2025-11-21 13:42:02 -0800

    Removed necessary hostile networks recipes

--- kubejs/server_scripts/recipes/removal.js
+++ kubejs/server_scripts/recipes/removal.js
@@ -83,6 +83,26 @@ let recipeRemoval = (/** @type {Internal.RecipesEventJS} */ event) => {
   event.remove({ id: "gtceu:assembly_line/high_performance_computing_array"})
   event.remove({ id: "gtceu:assembly_line/ultimate_battery"})
 
+  // Hostile Neural Networks
+  event.remove({ id: "hostilenetworks:sim_chamber" })
+  event.remove({ id: "hostilenetworks:loot_fabricator"})
+  event.remove({ id: "hostilenetworks:deep_learner"})
+  event.remove({ id: "hostilenetworks:framework" })
+
+  event.remove({ id: "hostilenetworks:living_matter/overworldian/rotten_flesh"})
+  event.remove({ id: "hostilenetworks:living_matter/overworldian/carrot"})
+  event.remove({ id: "hostilenetworks:living_matter/overworldian/gunpowder"})
+  event.remove({ id: "hostilenetworks:living_matter/overworldian/bone"})
+  event.remove({ id: "hostilenetworks:living_matter/overworldian/potato"})
+  event.remove({ id: "hostilenetworks:living_matter/overworldian/grass"})
+
+  event.remove({ id: "hostilenetworks:living_matter/hellish/nether_wart"})
+  event.remove({ id: "hostilenetworks:living_matter/hellish/soul_sand"})
+  event.remove({ id: "hostilenetworks:living_matter/hellish/ghast_tear"})
+
+  event.remove({ id: "hostilenetworks:living_matter/extraterrestrial/end_stone"})
+  event.remove({ id: "hostilenetworks:living_matter/extraterrestrial/chorus_fruit"})
+
   //GT / Railcraft Tool Specific
   toolsToRemove.forEach((tool) => {
     event.remove({ output: `gtceu:${tool}_shovel`, mod: "gtceu" })
